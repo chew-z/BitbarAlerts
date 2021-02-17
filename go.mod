95f02fb47e3e4edf13dbd6c7b488276c68a6df03
Robert J <chew-z@users.noreply.github.com>
2021-02-17 14:02:18 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module Bitbar
+
+go 1.16
+
+require (
+	github.com/johnmccabe/go-bitbar v0.5.0
+	github.com/joho/godotenv v1.3.0
+)
---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/johnmccabe/go-bitbar"
+	"github.com/joho/godotenv"
+)
+
+/*Quotes - ..
+ */
+type Quotes []Quote
+
+/*Quote - ..
+ */
+type Quote struct {
+	Symbol           string  `json:"_symbol"`
+	AskPrice         float64 `json:"_ask_price"`
+	BidPrice         float64 `json:"_bid_price"`
+	RefBidPrice      float64 `json:"_ref_bid_price"`
+	HighBidPrice     float64 `json:"_high_bid_price"`
+	LowBidPrice      float64 `json:"_low_bid_price"`
+	BidDayChange     float64 `json:"_bid_day_change"`
+	BidDayChangePcnt string  `json:"_bid_day_change_pcnt"`
+	QuoteTm          int64   `json:"_quote_tm"`
+	Pips             float64 `json:"_pips"`
+	PipsLot          float64 `json:"_pips_lot"`
+	Digits           float64 `json:"_digits"`
+	MonthMin         float64 `json:"_30d_min_bid_price"`
+	MonthMax         float64 `json:"_30d_max_bid_price"`
+}
+
+type displayQuote struct {
+	time          string
+	symbol        string
+	bid           float64
+	percentChange string
+	change        float64
+	webURL        string
+}
+
+var (
+	assets []string
+	apiURL string
+	webURL string
+	city   string
+	// http.Clients should be reused instead of created as needed.
+	client = &http.Client{
+		Timeout: 3 * time.Second,
+	}
+	userAgent = randUserAgent()
+)
+
+func init() {
+	if err := godotenv.Load("/Users/rrj/Projekty/Go/src/Bitbar/env.yaml"); err != nil {
+		log.Fatal("Error loading env file")
+	}
+	apiURL = os.Getenv("API_URL")
+	webURL = os.Getenv("WEB_URL")
+	city = os.Getenv("CITY")
+	assets = strings.Split(os.Getenv("ASSETS"), ":")
+}
+
+func main() {
+	app := bitbar.New()
+	// get all quotes in paralel
+	resultsChan := make(chan *displayQuote)
+	for _, asset := range assets {
+		go getQuote(asset, resultsChan)
+	}
+	defer func() {
+		close(resultsChan)
+	}()
+	results := 0
+	for {
+		quote := <-resultsChan
+		results++
+		l := fmt.Sprintf("%s: %.5g %s", quote.symbol, quote.bid, quote.percentChange)
+		line := app.StatusLine(l)
+		if quote.change < 0.0 {
+			line.Color("red")
+		} else {
+			line.Color("green")
+		}
+		// stop if we've received all quotes
+		if results == len(assets) {
+			break
+		}
+	}
+	app.Render()
+}
+
+/*getQuote
+ */
+func getQuote(asset string, ch chan<- *displayQuote) {
+	var q displayQuote
+	apiURL := fmt.Sprintf("%s%s.", apiURL, asset)
+	request, _ := http.NewRequest("GET", apiURL, nil)
+	request.Header.Set("User-Agent", userAgent)
+	if response, err := client.Do(request); err != nil {
+		log.Println(err.Error())
+	} else {
+		var body Quotes
+		json.NewDecoder(response.Body).Decode(&body)
+		tm := time.Unix(0, body[0].QuoteTm*int64(time.Millisecond))
+		location, _ := time.LoadLocation(city)
+		q.time = tm.In(location).Format("15:04")
+		q.symbol = asset
+		q.bid = body[0].BidPrice
+		q.change = body[0].BidDayChange
+		q.percentChange = body[0].BidDayChangePcnt
+		q.webURL = fmt.Sprintf("%s%s.", webURL, asset)
+	}
+	ch <- &q
+}
